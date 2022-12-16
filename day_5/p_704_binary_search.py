d3217417280efdc5f045057f90b7de4a8beae1d2
mduong1701 <minhmanlk0411@gmail.com>
2022-12-15 23:29:31 -0800

    Just do it

---
+++ day_5/p_704_binary_search.py
@@ -0,0 +1,14 @@
+def search(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        middle = (left + right) // 2
+        if nums[middle] == target:
+            return middle
+        elif nums[middle] < target:
+            left = middle + 1
+        else:
+            right = middle - 1
+
+    return -1
