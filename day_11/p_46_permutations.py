6f9b6871a45f2c962c705ecec42f4020692f97fd
mduong1701 <minhmanlk0411@gmail.com>
2023-01-12 19:38:38 -0800

    Just do it

---
+++ day_11/p_46_permutations.py
@@ -0,0 +1,16 @@
+def permute(nums):
+    result = []
+    n = len(nums)
+
+    def dfs(first):
+        if first == n:
+            result.append(nums[:])
+        
+        for i in range(first, n):
+            nums[first], nums[i] = nums[i], nums[first]
+            dfs(first + 1)
+            nums[first], nums[i] = nums[i], nums[first]
+
+
+    dfs(0)
+    return result
