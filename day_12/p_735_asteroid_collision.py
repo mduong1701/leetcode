bed1727260ac7502a8ef4a589c150b3289831c4b
mduong1701 <minhmanlk0411@gmail.com>
2023-01-14 11:43:04 -0800

    Just do it

--- day_12/p_735_asteroid_collision.py
+++ day_12/p_735_asteroid_collision.py
@@ -1,2 +1,19 @@
 def asteroidCollision(asteroids):
-    
+    stack = []
+
+    for asteroid in asteroids:
+
+        while stack and stack[-1] > 0 and asteroid < 0:
+            collision = stack[-1] + asteroid
+
+            if collision > 0: # 10 vs -5
+                asteroid = 0
+            elif collision < 0: # 10 vs -15
+                stack.pop()
+            else: # 10 vs -10
+                stack.pop()
+                asteroid = 0
+        if asteroid:
+            stack.append(asteroid)
+
+    return stack
