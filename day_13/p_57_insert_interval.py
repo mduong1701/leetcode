df6d3f1633f946bd35f69f1a3eac373fe087668e
mduong1701 <minhmanlk0411@gmail.com>
2023-01-15 14:50:44 -0800

    Just do it

--- day_13/p_57_insert_interval.py
+++ day_13/p_57_insert_interval.py
@@ -1,7 +1,7 @@
 def insert(intervals, newInterval):
     result = []
 
-    for i in len(intervals):
+    for i in range(len(intervals)):
         if newInterval[1] < intervals[i][0]:
             result.append(newInterval)
             return result + intervals[i:]
