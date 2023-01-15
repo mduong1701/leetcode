65bf4c31b92e16100f4cb7748ce87130a6c81bfd
mduong1701 <minhmanlk0411@gmail.com>
2023-01-15 14:49:10 -0800

    Just do it

--- day_13/p_57_insert_interval.py
+++ day_13/p_57_insert_interval.py
@@ -3,5 +3,13 @@ def insert(intervals, newInterval):
 
     for i in len(intervals):
         if newInterval[1] < intervals[i][0]:
-            return newInterval + intervals[i:]
-        elif 
+            result.append(newInterval)
+            return result + intervals[i:]
+        elif newInterval[0] > intervals[i][1]:
+            result.append(intervals[i])
+        else:
+            newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+    
+    result.append(newInterval)
+
+    return result
