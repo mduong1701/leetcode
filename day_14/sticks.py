62cd9fbd310b1c0c1cec3b2d6b379580480b7753
mduong1701 <minhmanlk0411@gmail.com>
2023-01-17 13:55:54 -0800

    Just do it

---
+++ day_14/sticks.py
@@ -0,0 +1,16 @@
+def stickNum(sticks, k):
+
+    resultArray = [k + 1] * (k + 1)
+    resultArray[0] = 0
+
+    for i in range(1, k + 1):
+        for stick in sticks:
+            if i >= stick:
+                resultArray[i] = min(resultArray[i], 1 + resultArray[i - stick])
+    
+    if resultArray[-1] == k + 1:
+        return -1
+    
+    return resultArray[-1]
+
+print(stickNum([1,5,12], 13))
