ac74663d161e258e2f77ba28a95174fc4840b263
mduong1701 <minhmanlk0411@gmail.com>
2023-01-28 11:07:08 -0800

    Just do it

--- day_16/p_46.py
+++ day_16/p_46.py
@@ -1,6 +1,17 @@
 def permute(nums):
     result = []
 
+    def dfs(array, permutation):
+        if not array and permutation:
+            result.append(permutation)
+        else:
+            for i in range(len(array)):
+                newPermutation = permutation.append(array[i])
+                newArray = array[:i] + array[i+1:]
+                dfs(newArray, newPermutation)
 
+    dfs(nums, [])
 
     return result
+
+print(per)
