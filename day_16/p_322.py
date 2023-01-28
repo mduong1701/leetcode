8049e367b0144245c802ae7c332995efe1a503c4
mduong1701 <minhmanlk0411@gmail.com>
2023-01-28 12:16:33 -0800

    Just do it

--- day_16/p_322.py
+++ day_16/p_322.py
@@ -3,4 +3,8 @@ def coinChange(coins, amount):
     result[0] = 0
 
     for coin in coins:
-        
+        for i in range(coin, len(result)):
+            result[i] = min(result[i], 1 + result[i - coin])
+    
+    return result[-1] if result[-1] != amount else -1
+
