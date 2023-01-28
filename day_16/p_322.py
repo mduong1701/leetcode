bdebd374165cf6244c4c303d6396f74b76bbcde1
mduong1701 <minhmanlk0411@gmail.com>
2023-01-28 12:22:22 -0800

    Just do it

--- day_16/p_322.py
+++ day_16/p_322.py
@@ -1,10 +1,12 @@
 def coinChange(coins, amount):
-    result = [amount for _ in range(amount + 1)]
+    result = [amount + 1 for _ in range(amount + 1)]
     result[0] = 0
 
     for coin in coins:
         for i in range(coin, len(result)):
             result[i] = min(result[i], 1 + result[i - coin])
     
-    return result[-1] if result[-1] != amount else -1
+    return result[-1] if result[-1] != amount + 1 else -1
 
+print(coinChange([1,2,5], 11))
+print(coinChange([2], 3))
