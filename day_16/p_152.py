11483fa97bb07a54d76f749969cdfb4e51d6fc15
mduong1701 <minhmanlk0411@gmail.com>
2023-01-28 11:55:38 -0800

    Just do it

--- day_16/p_152.py
+++ day_16/p_152.py
@@ -3,7 +3,17 @@ from winreg import REG_RESOURCE_REQUIREMENTS_LIST
 
 def maxProduct(nums):
     result = max(nums)
+    curMax = 1
+    curMin = 1
 
-    
+    for num in nums:
+        if num == 0:
+            curMax = 1
+            curMin = 1
+        else:
+            temp = curMax
+            curMax = max(curMax * num, curMin * num, num)
+            curMin = min(temp * num, curMin * num, num)
+            result = max(result, curMax)
 
     return result
