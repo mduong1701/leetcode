88f02f5731dbce77753c4a7aed32ff045b87013d
mduong1701 <minhmanlk0411@gmail.com>
2023-01-27 20:04:06 -0800

    Just do it

--- day_15/p_15_3sum.py
+++ day_15/p_15_3sum.py
@@ -1,5 +1,27 @@
 def threeSum(nums):
     nums.sort()
-    return nums
+
+    visited = set()
+
+    result = []
+
+    for i in range(len(nums) - 2):
+        if nums[i] in visited:
+            continue
+
+        visited.add(nums[i])
+
+        numSet = set()
+        for j in range(i + 1, len(nums)):
+            dif = 0 - nums[i] - nums[j]
+            if dif not in numSet:
+                numSet.add(nums[j])
+            else:
+                result.append([nums[i], nums[j], dif])
+    
+    return result
+
+
+
 
 print(threeSum([-1,0,1,2,-1,-4]))
