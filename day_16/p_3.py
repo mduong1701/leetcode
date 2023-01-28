033f62610c5d72e7b02cc0fd0ec6ccc185dce4a1
mduong1701 <minhmanlk0411@gmail.com>
2023-01-28 11:29:35 -0800

    Just do it

--- day_16/p_3.py
+++ day_16/p_3.py
@@ -1,8 +1,15 @@
 def longest(s):
     result = 0
+    left = 0
+    uniqueCharacters = set()
 
-
-
-
+    for right in range(len(s)):
+        while s[right] in uniqueCharacters:
+            uniqueCharacters.remove(s[left])
+            left += 1
+        uniqueCharacters.add(s[right])
+        result=max(result, len(s[left:right+1]))
 
     return result
+
+print(longest("abcabcbb"))
