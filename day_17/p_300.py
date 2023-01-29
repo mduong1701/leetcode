e183489394be9201ee472b6f823347c8faf21013
mduong1701 <minhmanlk0411@gmail.com>
2023-01-29 13:21:13 -0800

    Just do it

--- day_17/p_300.py
+++ day_17/p_300.py
@@ -1,8 +1,4 @@
 def longestSub(nums):
-    result = 1
+    result = [ 1 for _ in range(len(nums))]
 
-    for i, num in enumerate(nums):
-        if i > 0 and nums[i - 1] >= nums[i]:
-            continue
 
-        result += 1
