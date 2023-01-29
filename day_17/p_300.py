6997ef1a75c0a90fd4a3089a8d9f89deed2cd7f9
mduong1701 <minhmanlk0411@gmail.com>
2023-01-29 13:26:16 -0800

    Just do it

--- day_17/p_300.py
+++ day_17/p_300.py
@@ -2,5 +2,11 @@ def longestSub(nums):
     result = [ 1 for _ in range(len(nums))]
 
     for i in range(len(result)):
-        
+        for j in range(i):
+            if nums[j] < nums[i]:
+                result[i] = max(1, 1 + result[j])
+
+    return max(result)
+
+
 
