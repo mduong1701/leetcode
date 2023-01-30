33d97110ac861da46dc7bdff7de006d059415beb
mduong1701 <minhmanlk0411@gmail.com>
2023-01-30 09:10:05 -0800

    Just do it

--- day_18/p_207.py
+++ day_18/p_207.py
@@ -1,2 +1,7 @@
 def courseSchedule(numCourses, prerequisites):
+    # Create a dictionary 
+    courseMap = { i : [] for i in range(numCourses)}
+    # Build the dictionary
+    for second, first in prerequisites:
+        courseMap[second].append(first)
     
