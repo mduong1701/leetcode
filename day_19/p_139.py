1e833e083dc5ef1a83c7e26f0f5ac23ee92017c0
mduong1701 <minhmanlk0411@gmail.com>
2023-01-31 10:27:40 -0800

    Just do it

--- day_19/p_139.py
+++ day_19/p_139.py
@@ -2,4 +2,11 @@ def wordBreak(s, wordDict):
     dp = [False for _ in range(len(s) + 1)]
     dp[-1] = True
 
+    for i in range(len(s) - 1, -1, -1):
+        for word in wordDict:
+            if i + len(word) <= len(s) and s[i : i + len(word)] == word:
+                dp[i] = dp[i + len(word)]
+            if dp[i]:
+                break
     
+    return dp[0]
