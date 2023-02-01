3c10cf6163b410dd0be239c25eb45e6f8dc0a1fa
mduong1701 <minhmanlk0411@gmail.com>
2023-02-01 15:42:27 -0800

    Just do it

--- day_20.py/p_22.py
+++ day_20.py/p_22.py
@@ -3,4 +3,20 @@ def generateParenthesis(n):
     result = []
 
     def backtrack(numO, numC):
+        if numO == numC == n:
+            result.append("".join(stack))
+            return
         
+        if numO < n:
+            stack.append("(")
+            backtrack(numO + 1, numC)
+            stack.pop()
+
+        if numC < numO:
+            stack.append(")")
+            backtrack(numO, numC + 1)
+            stack.pop()
+
+    backtrack(0, 0)
+
+    return result
