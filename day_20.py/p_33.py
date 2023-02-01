b9e613c7fc2aa4760dbb06f56e105c52a702ed1f
mduong1701 <minhmanlk0411@gmail.com>
2023-02-01 15:05:09 -0800

    Just do it

--- day_20.py/p_33.py
+++ day_20.py/p_33.py
@@ -5,3 +5,15 @@ def search(nums, target):
     while left <= right:
         middle = (left + right) // 2
         
+        if nums[middle] == target:
+            return middle
+
+        if nums[middle] >= nums[left]:
+            if target > nums[middle] or target < nums[left]:
+                left = middle + 1
+            else:
+                right = left - 1
+        
+        else:
+            if target < nums[middle] or target > nums[right]:
+                right = middle - 1
