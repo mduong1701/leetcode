66469967ed148cb6f4b00648cf00574c5c6f1792
mduong1701 <minhmanlk0411@gmail.com>
2023-02-03 13:55:04 -0800

    Just do it

--- day_21/p_200.py
+++ day_21/p_200.py
@@ -13,13 +13,20 @@ def numIslands(grid):
     islands = 0
 
     def bfs(r, c):
+
         DIRECTIONS = [[1,0], [-1, 0], [0,1], [0,-1]]
+        
         for dR, dC in DIRECTIONS:
             newRow = r + dR
             newColumn = c + dC
             if  newRow >= 0 and \
-                newRow < ROWS \
-
+                newRow < ROWS and \
+                newColumn >= 0 and \
+                newColumn < COLUMNS and \
+                grid[newRow][newColumn] == "1" and \
+                (newRow, newColumn) not in visited:
+                    visited.add((newRow, newColumn))
+                    q.popleft()
 
     for row in range(ROWS):
         for column in range(COLUMNS):
