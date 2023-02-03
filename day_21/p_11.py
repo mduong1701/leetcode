4c71311d3ae104d86bf4ff5575b9ec305919044a
mduong1701 <minhmanlk0411@gmail.com>
2023-02-03 14:41:27 -0800

    Just do it

--- day_21/p_11.py
+++ day_21/p_11.py
@@ -2,7 +2,16 @@ def maxArea(height):
     
     left = 0
     right = len(height) - 1
-    area = 0
+    maxArea = 0
 
     while left < right:
-        
+        area = (right - left) * min(height[left], height[right])
+        maxArea = max(maxArea, area)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return maxArea
+
