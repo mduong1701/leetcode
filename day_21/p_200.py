79e3cda465ae964ae332e88c859125078159022f
mduong1701 <minhmanlk0411@gmail.com>
2023-02-03 13:41:31 -0800

    Just do it

--- day_21/p_200.py
+++ day_21/p_200.py
@@ -1,4 +1,6 @@
 def numIslands(grid):
+    if not grid:
+        return 0
 
     ROWS = len(grid)
     COLUMNS = len(grid[0])
@@ -6,4 +8,13 @@ def numIslands(grid):
     visited = set()
     islands = 0
 
-    
+    def bfs(r, c):
+        pass
+
+    for row in range(ROWS):
+        for column in range(COLUMNS):
+            if grid[row][column] == "1" and (row, column) not in visited:
+                bfs(row, column)
+                islands += 1
+
+    return islands
