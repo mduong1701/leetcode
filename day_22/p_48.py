682689b3fa6b98426be811a7745f7c4bd807bb26
mduong1701 <minhmanlk0411@gmail.com>
2023-02-04 09:51:27 -0800

    Just do it

--- day_22/p_48.py
+++ day_22/p_48.py
@@ -1,8 +1,14 @@
 def rotate(matrix):
     
     left = 0
-    right = len(matrix)
+    right = len(matrix) - 1
     top = 0
-    bottom = len(matrix)
+    bottom = len(matrix) - 1
 
-    
+    while left <= right:
+        for i in range(left, right + 1):
+            temp = matrix[top][i]
+            matrix[top][i] = matrix[bottom][i]
+            matrix[bottom][i] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top][right - i]
+            matrix[top][right - i] = temp
