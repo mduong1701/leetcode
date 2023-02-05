68ef28b7e494282c661f4e28d7733b68631ec31b
mduong1701 <minhmanlk0411@gmail.com>
2023-02-05 12:41:00 -0800

    Just do it

--- day_23/p_47.py
+++ day_23/p_47.py
@@ -1,3 +1,14 @@
 def permute(nums):
 
-    
+    result = []
+    currentA = []
+
+    count = { n : 0 for n in nums}
+    for num in nums:
+        count[num] += 1
+
+    def dfs():
+        if len(currentA) == len(nums):
+            result.append(currentA)
+
+        
