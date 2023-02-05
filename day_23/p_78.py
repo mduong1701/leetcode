ea3c6f57ed2181c1741a50fe59876906ef0d7d34
mduong1701 <minhmanlk0411@gmail.com>
2023-02-05 11:34:44 -0800

    Just do it

--- day_23/p_78.py
+++ day_23/p_78.py
@@ -1,4 +1,16 @@
 def subsets(nums):
     result = []
+    subset = []
 
-    
+    def dfs(i):
+        if i >= len(nums):
+            result.append(subset.copy())
+            return
+
+        subset.append(nums[i])
+        dfs(i + 1)
+        subset.pop()
+        dfs(i + 1)
+
+    dfs(0)
+    return result
