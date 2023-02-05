9d05b83c2351980242bc34c568f4e44f93424bf7
mduong1701 <minhmanlk0411@gmail.com>
2023-02-05 12:08:58 -0800

    Just do it

--- day_23/p_39.py
+++ day_23/p_39.py
@@ -3,4 +3,12 @@ def combinationSum(candidates, target):
     currentArray = []
 
     def dfs(i):
-        
+        if sum(currentArray) > target:
+            return
+        if sum(currentArray) == target:
+            result.append(currentArray.copy())
+
+        currentArray.append(candidates[i])
+        dfs(i)
+        currentArray.pop()
+        dfs(i + 1)
