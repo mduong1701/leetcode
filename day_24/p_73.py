5046fdc0ea8d7961196648c5734db414a1072535
mduong1701 <minhmanlk0411@gmail.com>
2023-02-06 10:57:57 -0800

    Just do it

--- day_24/p_73.py
+++ day_24/p_73.py
@@ -3,4 +3,18 @@ def setZeroes(matrix):
     ROWS = len(matrix)
     COLUMNS = len(matrix[0])
 
+    for row in range(ROWS):
+        for column in range(COLUMNS):
+            if matrix[row][column] == 0:
+                matrix[0][column] = 0
+                if row > 0:
+                    matrix[row][0] = 0
+                else:
+                    rowZero = True
+
+    for row in range(1, ROWS):
+        for column in range(1, COLUMNS):
+            if matrix[0][column] == 0 or matrix[row][0] == 0:
+                matrix[row][column] = 0
+            
     
