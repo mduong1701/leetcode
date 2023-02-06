6023bc4161f2466158ed4bdf692a380c2cfd895c
mduong1701 <minhmanlk0411@gmail.com>
2023-02-06 10:10:31 -0800

    Just do it

--- day_24/p_1143.py
+++ day_24/p_1143.py
@@ -3,5 +3,10 @@ def longest(text1, text2):
     len2 = len(text2)
     dp = [[0 for _ in range(len2 + 1)] for _ in range(len1 + 1)]
 
-    
+    for i in range(len1 - 1, -1, -1):
+        for j in range(len2 - 1, -1, -1):
+            if text1[i] == text2[j]:
+                dp[i][j] = 1 + dp[i + 1][j + 1]
+            else:
+                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
 
