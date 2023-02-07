5d227c5113d249ebd0dbbd5ac617e19240a5e5d3
mduong1701 <minhmanlk0411@gmail.com>
2023-02-06 16:00:16 -0800

    Just do it

--- day_24/p_210.py
+++ day_24/p_210.py
@@ -1,2 +1,33 @@
 def findOrder(numCourses, prerequisites):
-    
+    prerequisiteMap = { c : [] for c in range(numCourses)}
+
+    for course, first in prerequisites:
+        prerequisiteMap[course].append(first)
+
+    visited = set()
+    cycle = set()
+    result = []
+
+    def dfs(course):
+        if course in cycle:
+            return False
+        if course in visited:
+            return True
+        
+        cycle.add(course)
+        
+        for pre in prerequisiteMap[course]:
+            if dfs(pre) == False:
+                return False
+
+        cycle.remove(course)
+        visited.add(course)
+        result.append(course)
+        
+        return True
+
+    for eachCourse in range(numCourses):
+        if dfs(eachCourse) == False:
+            return []
+
+    return result
