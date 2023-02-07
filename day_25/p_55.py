e68bd87d15ead4c47318fdcee8c5aab8fe8fdc0c
mduong1701 <minhmanlk0411@gmail.com>
2023-02-07 09:58:54 -0800

    Just do it

--- day_25/p_55.py
+++ day_25/p_55.py
@@ -1,1 +1,14 @@
 def canJump(nums):
+    length = len(nums)
+
+    if length == 1 and nums[0] == 0:
+        return True
+
+    i = 0
+
+    while i <= length:
+        i += nums[i]
+        if i == length:
+            return True
+
+    return False
