3a9889f126f298372bc7bc12009663f797a71080
mduong1701 <minhmanlk0411@gmail.com>
2023-02-06 16:41:56 -0800

    Just do it

--- day_24/p_105.py
+++ day_24/p_105.py
@@ -1,2 +1,11 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 def buildTree(preorder, inorder):
-    
+    if not preorder or not inorder:
+        return None
+
+    root = TreeNode()
