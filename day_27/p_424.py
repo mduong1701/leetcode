7c40879aac0877a2f684abc5b37d0127597f95f6
Man Duong <mduong8911@hotmail.com>
2023-02-09 09:32:47 -0800

    Just do it

--- day_27/p_424.py
+++ day_27/p_424.py
@@ -6,3 +6,12 @@ def character(s, k):
 
     for right in range(len(s)):
         count[s[right]] = 1 + count.get(s[right], 0)
+        maxF = max(maxF, count[s[right]])
+
+        while (right - left + 1) - maxF > k:
+            count[s[left]] -= 1
+            left += 1
+
+        result = max(result, right - left + 1)
+
+    return result
