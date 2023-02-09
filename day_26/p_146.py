4269f8b57ff4c0935c80029c4e83058e35f746c4
Man Duong <mduong8911@hotmail.com>
2023-02-08 21:43:48 -0800

    Just do it

---
+++ day_26/p_146.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        pass
+
+    def get(self, key: int) -> int:
+        pass
+
+    def put(self, key: int, value: int) -> None:
+        pass
