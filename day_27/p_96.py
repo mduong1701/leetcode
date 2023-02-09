b8d63a1ff8c0881f909847fc348ec2bb3224bda8
Man Duong <mduong8911@hotmail.com>
2023-02-09 10:19:55 -0800

    Just do it

--- day_27/p_96.py
+++ day_27/p_96.py
@@ -1,4 +1,14 @@
 def numTrees(n):
 
     dp = [1 for _ in range(n + 1)]
-    
+
+    for numNodes in range(2, n + 1):
+        total = 0
+        for root in range(1, numNodes + 1):
+            left = root - 1
+            right = numNodes - root
+            total += dp[left] + dp[right]
+        dp[numNodes] = total
+
+    return dp[-1]
+
