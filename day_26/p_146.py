8decda8e3018c8ae231acbe4ca5a4fbeea4103c5
Man Duong <mduong8911@hotmail.com>
2023-02-08 21:46:19 -0800

    Just do it

--- day_26/p_146.py
+++ day_26/p_146.py
@@ -2,11 +2,18 @@ class Node:
     def __init__(self, key, value):
         self.key = key
         self.value = value
+        self.prev = None
+        self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        pass
+        self.capacity = capacity
+        # key : Node
+        self.cache = {}
+        # Double linked list
+        self.left = Node(0,0)
+        self.right = Node(0,0)
 
     def get(self, key: int) -> int:
         pass
