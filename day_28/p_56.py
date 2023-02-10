f53ef7ec65b9716455ed7d569ddc181b85615b2c
Man Duong <mduong8911@hotmail.com>
2023-02-10 10:51:33 -0800

    Just do it

--- day_28/p_56.py
+++ day_28/p_56.py
@@ -2,4 +2,7 @@ def merge(intervals):
     result = [intervals[0]]
 
     for start, end in intervals[1:]:
-        
+        if result[-1][1] > start:
+            result[-1][1] = max(result[-1][1], end)
+        else:
+            result.append([start, end])
