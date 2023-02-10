158b0905f28cc7ed5905b53d9ad53faadc0615d4
Man Duong <mduong8911@hotmail.com>
2023-02-10 11:17:49 -0800

    Just do it

--- day_28/p_213.py
+++ day_28/p_213.py
@@ -3,9 +3,15 @@ def rob(nums):
 
 def helper(nums):
     # 0      0     1     2     3      4
-    #rob1  rob2
+    #rob1  rob2   num
+    #      rob1   rob2  num
 
     rob1 = 0
     rob2 = 0
 
-    
+    for num in nums:
+        newRob = max(rob1 + num, rob2)
+        rob1 = rob2
+        rob2 = newRob
+
+    return rob2
