520f159fdc7367cef9318a5c85040c83320e37fa
Man Duong <mduong8911@hotmail.com>
2023-02-09 21:16:27 -0800

    Just do it

--- day_27/p_54.py
+++ day_27/p_54.py
@@ -5,4 +5,24 @@ def spiral(matrix):
     top = 0
     bottom = len(matrix) - 1
 
-    
+    result = []
+
+    while left <= right and top <= bottom:
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+        
+        if len(result) == (len(matrix) * len(matrix[0])):
+            break
+
+        for i in range(right, left - 1, -1):
+            result.append(matrix[bottom][i])
+        bottom -= 1
+
+        for i in range(bottom, top - 1, -1):
+            result.append(matrix[i][left])
+        left += 1
