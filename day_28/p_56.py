726b7ef912f2eadf346349e8d7e8c744fd6f3770
Man Duong <mduong8911@hotmail.com>
2023-02-10 10:54:54 -0800

    Just do it

--- day_28/p_56.py
+++ day_28/p_56.py
@@ -1,8 +1,10 @@
 def merge(intervals):
+
+    intervals.sort(key = lambda inter : inter[0])
     result = [intervals[0]]
 
     for start, end in intervals[1:]:
-        if result[-1][1] > start:
+        if result[-1][1] >= start:
             result[-1][1] = max(result[-1][1], end)
         else:
             result.append([start, end])
