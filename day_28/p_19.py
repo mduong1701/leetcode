d29bf22ac8012eb7169820aa65189953abc40bdc
Man Duong <mduong8911@hotmail.com>
2023-02-10 11:41:39 -0800

    Just do it

--- day_28/p_19.py
+++ day_28/p_19.py
@@ -3,4 +3,20 @@ class ListNode:
         self.val = val
         self.next = next
 
-        
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode()
+        left = dummy
+        dummy.next = head
+        right = head
+
+        for _ in range(n):
+            right = right.next
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+
+        return dummy.next
