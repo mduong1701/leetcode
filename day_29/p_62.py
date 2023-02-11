edb4dfa0c26ee206743907ff7407b5a687628a96
Man Duong <mduong8911@hotmail.com>
2023-02-11 10:42:37 -0800

    Just do it

--- day_29/p_62.py
+++ day_29/p_62.py
@@ -1,8 +1,12 @@
 def uniquePaths(self, m, n):
     dp = [1 for _ in range(n)]
 
-    for _ in range(m - 1):
+    for i in range(m - 1):
         temp = [1 for _ in range(n)]
-        
+
+        for j in range(n - 2, -1, -1):
+            temp[j] = temp[j + 1] + dp[j]
+
+        dp = temp
 
     return dp[0]
