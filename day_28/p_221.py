886e8a19464fa9d283744d0538ccae81237e55a0
Man Duong <mduong8911@hotmail.com>
2023-02-10 18:47:02 -0800

    Just do it

--- day_28/p_221.py
+++ day_28/p_221.py
@@ -7,4 +7,12 @@ def maximalSquare(matrix):
         if row >= ROWS or column >= COLUMNS:
             return 0
 
-        
+        if (row, column) not in cache:
+
+            down = dfs(row + 1, column)
+            right = dfs(row, column + 1)
+            diag = dfs(row + 1, column + 1)
+
+            
+
+        return cache[(row, column)]
