d2900b92175506e27c4af5dde0ef8ca120731d0b
Man Duong <mduong8911@hotmail.com>
2023-02-10 18:50:14 -0800

    Just do it

--- day_28/p_221.py
+++ day_28/p_221.py
@@ -13,6 +13,9 @@ def maximalSquare(matrix):
             right = dfs(row, column + 1)
             diag = dfs(row + 1, column + 1)
 
-            
+            cache[(row, column)] = 0
+
+            if matrix[row][column] == "1":
+                cache[(row, column)] = 1 + min(down, right, diag)
 
         return cache[(row, column)]
