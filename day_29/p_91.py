5ec8dfc899fb9b012de3e925775bdfef8ae5e9b2
Man Duong <mduong8911@hotmail.com>
2023-02-11 09:54:33 -0800

    Just do it

--- day_29/p_91.py
+++ day_29/p_91.py
@@ -3,4 +3,9 @@ def numDecodings(s):
     dp[-1] = 1
 
     for i in range(len(s) - 1, -1, -1):
-        
+        if s[i] != "0":
+            dp[i] = dp[i + 1]
+        if (i + 1 < len(s)) and (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456")):
+            dp[i] += dp[i+2]
+
+    return dp[0]
