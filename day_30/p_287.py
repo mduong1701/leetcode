5ffe2c13e7fcb42904be5e442447962447ac5e93
Man Duong <mduong8911@hotmail.com>
2023-02-13 10:58:51 -0800

    Just do it

--- day_30/p_287.py
+++ day_30/p_287.py
@@ -1,4 +1,4 @@
-def findDuplicate(self, nums):
+def findDuplicate(nums):
     slow = 0
     fast = 0
 
@@ -15,8 +15,8 @@ def findDuplicate(self, nums):
     while True:
         slow = nums[slow]
         fast = nums[fast]
-        fast = nums[fast]
         if slow == fast:
             return slow
 
-        
+print(findDuplicate([1,3,4,2,2]))
+print(findDuplicate([3,1,3,4,2]))
