41bb9d6fd225817dfe1e548cb8db1b878d5d40f3
Man Duong <mduong8911@hotmail.com>
2023-02-13 13:42:30 -0800

    Just do it

--- day_30/p_253.py
+++ day_30/p_253.py
@@ -1,3 +1,21 @@
 def minMeetingRooms(intervals):
     startTimes = sorted([ interval[0] for interval in intervals])
     endTimes = sorted([ interval[1] for interval in intervals])
+
+    start = 0
+    end = 0
+
+    result = 0
+    count = 0
+
+    while start < len(startTimes):
+        if startTimes[start] < endTimes[end]:
+            count += 1
+            start += 1
+        else:
+            count -= 1
+            end += 1
+        
+        result = max(result, count)
+
+    return result
