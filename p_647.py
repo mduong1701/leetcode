8eeff2a443a3657bda578b3f8a318599fe0d0d4d
Man Duong <mduong8911@hotmail.com>
2023-02-16 21:11:25 -0800

    Just do it

--- p_647.py
+++ p_647.py
@@ -3,4 +3,18 @@ class Solution:
         result = 0
         
         for i in range(len(s)):
-            
+            left = i
+            right = i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                result += 1
+                left -= 1
+                right += 1
+
+            left = i
+            right = i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                result += 1
+                left -= 1
+                right += 1
+
+        return result
