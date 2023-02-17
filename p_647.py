af0df1bc28a78a3950d0b124154531cb1d327a1e
Man Duong <mduong8911@hotmail.com>
2023-02-16 21:12:19 -0800

    Just do it

--- p_647.py
+++ p_647.py
@@ -1,7 +1,6 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         result = 0
-        
         for i in range(len(s)):
             left = i
             right = i
@@ -9,12 +8,11 @@ class Solution:
                 result += 1
                 left -= 1
                 right += 1
-
             left = i
             right = i + 1
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 result += 1
                 left -= 1
                 right += 1
-
         return result
+
