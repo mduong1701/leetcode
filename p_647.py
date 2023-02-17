2a8477cdce80572a7f694a7172ae89b6a37905d6
Man Duong <mduong8911@hotmail.com>
2023-02-16 21:15:53 -0800

    Just do it

--- p_647.py
+++ p_647.py
@@ -1,18 +1,33 @@
 class Solution:
+    # def countSubstrings(self, s: str) -> int:
+    #     result = 0
+    #     for i in range(len(s)):
+    #         left = i
+    #         right = i
+    #         while left >= 0 and right < len(s) and s[left] == s[right]:
+    #             result += 1
+    #             left -= 1
+    #             right += 1
+    #         left = i
+    #         right = i + 1
+    #         while left >= 0 and right < len(s) and s[left] == s[right]:
+    #             result += 1
+    #             left -= 1
+    #             right += 1
+    #     return result
+
     def countSubstrings(self, s: str) -> int:
         result = 0
+
         for i in range(len(s)):
-            left = i
-            right = i
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                result += 1
-                left -= 1
-                right += 1
-            left = i
-            right = i + 1
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                result += 1
-                left -= 1
-                right += 1
+            result += self.isPal(s, i, i)
+            result += self.isPal(s, i, i + 1)
         return result
 
+    def isPal(self, s, left, right):
+        count = 0
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            count += 1
+            left -= 1
+            right += 1
+        return count
