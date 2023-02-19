30e20bc0390e71cff6fa33cf4cfea123a07ec5ff
Man Duong <mduong8911@hotmail.com>
2023-02-19 08:58:47 -0800

    Just do it

--- day_32/p_208.py
+++ day_32/p_208.py
@@ -18,11 +18,23 @@ class Trie:
             if character not in current.children:
                 current.children[character] = TrieNode()
             current = current.children[character]
-        
+        current.endOfWord = True
 
 
-    def search(self, word: str) -> bool:
-        
+    def search(self, word):
+        current = self.root
 
-    def startsWith(self, prefix: str) -> bool:
-        
+        for character in word:
+            if character not in current.children:
+                return False
+            current = current.children[character]
+        return current.endOfWord
+
+    def startsWith(self, prefix):
+        current = self.root
+
+        for character in prefix:
+            if character not in current.children:
+                return False
+            current = current.children[character]
+        return True
