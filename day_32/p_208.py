72e5e42962073f4024349d666e7b8f7072914e81
Man Duong <mduong8911@hotmail.com>
2023-02-19 08:48:42 -0800

    Just do it

---
+++ day_32/p_208.py
@@ -0,0 +1,18 @@
+class TrieNode():
+    def  __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class Trie:
+
+    def __init__(self):
+        
+
+    def insert(self, word: str) -> None:
+        
+
+    def search(self, word: str) -> bool:
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        
