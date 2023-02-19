e2b593b95b9f279c9b6f0cc7f38b1aacb58f7ea4
Man Duong <mduong8911@hotmail.com>
2023-02-19 14:18:02 -0800

    Just do it

--- day_32/p_560.py
+++ day_32/p_560.py
@@ -2,4 +2,12 @@ class Solution:
     def subarraySum(self, nums, k):
         result = 0
         dic = { 0 : 1}
-        
+        preSum = 0
+
+        for num in nums:
+            preSum += num
+            diff = preSum - k
+            result += dic.get(diff, 0)
+            dic[preSum] = 1 + dic.get(preSum, 0)
+
+        return result
