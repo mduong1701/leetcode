6dc1e0f74e246644e0ee46c1e4c018984cf58eb4
Man Duong <mduong8911@hotmail.com>
2023-02-19 14:15:51 -0800

    Just do it

--- day_32/p_560.py
+++ day_32/p_560.py
@@ -1,15 +1,5 @@
 class Solution:
     def subarraySum(self, nums, k):
-        i = 1
-        while i < len(nums):
-            nums[i] += nums[i - 1]
-
-        count = 0
-        visited = set()
-
-        for i in range(len(nums)):
-            if nums[i] == k or (k - nums[i] in visited):
-                count += 1
-            visited.add(nums[i])
-
-        return count
+        result = 0
+        dic = { 0 : 1}
+        
