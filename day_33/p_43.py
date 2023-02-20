e25ee126e78507866f35755030ebe70c2418ad7e
Man Duong <mduong8911@hotmail.com>
2023-02-20 14:13:21 -0800

    Just do it

--- day_33/p_43.py
+++ day_33/p_43.py
@@ -1,3 +1,8 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+        if "0" in [num1, num2]:
+            return "0"
+
+        result = [ 0 for _ in range(len(num1) + len(num2))]
+
         
