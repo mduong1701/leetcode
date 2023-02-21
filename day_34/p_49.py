f65ff1627b68919c631bf43d25f8c63d1195cbdc
Man Duong <mduong8911@hotmail.com>
2023-02-21 10:14:11 -0800

    Just do it

--- day_34/p_49.py
+++ day_34/p_49.py
@@ -1,3 +1,10 @@
+from collections import defaultdict
+
+
 class Solution:
     def groupAnagrams(self, strs):
-        
+        result = defaultdict(list)
+
+        for string in strs:
+            countArray = [0 for _ in range(26)]
+            
