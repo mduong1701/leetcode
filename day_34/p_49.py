9ed343122e148a2e2b5b07160595cf46935f6b46
Man Duong <mduong8911@hotmail.com>
2023-02-21 10:17:23 -0800

    Just do it

--- day_34/p_49.py
+++ day_34/p_49.py
@@ -9,8 +9,6 @@ class Solution:
             countArray = [0 for _ in range(26)]
             for character in string:
                 countArray[ord(character) - ord("a")] += 1
-
             result[tuple(countArray)].append(string)
 
-        
-        
+        return result.values
