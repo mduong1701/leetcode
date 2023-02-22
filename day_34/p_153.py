781c6522386378e47191420c695788fc6fc6b080
Man Duong <mduong8911@hotmail.com>
2023-02-21 21:38:33 -0800

    Just do it

--- day_34/p_153.py
+++ day_34/p_153.py
@@ -9,3 +9,13 @@ class Solution:
             if nums[left] < nums[right]:
                 result = min(result, nums[left])
                 break
+
+            middle = (left + right) // 2
+            result = min(result, nums[middle])
+
+            if nums[left] >= nums[middle]:
+                left = middle + 1
+            else:
+                right = middle - 1
+
+        return result
