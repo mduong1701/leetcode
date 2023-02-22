2b63fa1761e6fd2b02eafb5b7a54682e2fe08326
Man Duong <mduong8911@hotmail.com>
2023-02-21 21:42:21 -0800

    Just do it

--- day_34/p_153.py
+++ day_34/p_153.py
@@ -3,7 +3,7 @@ class Solution:
         result = nums[0]
 
         left = 0
-        right = len(nums)
+        right = len(nums) - 1
 
         while left <= right:
             if nums[left] < nums[right]:
@@ -13,7 +13,7 @@ class Solution:
             middle = (left + right) // 2
             result = min(result, nums[middle])
 
-            if nums[left] >= nums[middle]:
+            if nums[middle] >= nums[left]:
                 left = middle + 1
             else:
                 right = middle - 1
