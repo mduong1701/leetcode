291fee545e830b3dd720af684c58fa6e045515e3
Man Duong <mduong8911@hotmail.com>
2023-02-23 23:12:32 -0800

    Just do it

--- day_36/p_120.py
+++ day_36/p_120.py
@@ -3,5 +3,9 @@ class Solution:
         dp = [0 for _ in range(len(triangle[-1]) + 1)]
 
         for i in range(len(triangle), 0, -1):
-            temp = triangle[i - 1]
+            temp = triangle[i - 1].copy()
+
+            for j in range(len(temp)):
+                temp[j] += min(dp[j], dp[j + 1])
+            
             
