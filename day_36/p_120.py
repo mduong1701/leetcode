7feb7f06c68cee603292204be8513964b2cd7af0
Man Duong <mduong8911@hotmail.com>
2023-02-23 23:10:01 -0800

    Just do it

--- day_36/p_120.py
+++ day_36/p_120.py
@@ -2,4 +2,6 @@ class Solution:
     def minimumTotal(self, triangle):
         dp = [0 for _ in range(len(triangle[-1]) + 1)]
 
-        
+        for i in range(len(triangle), 0, -1):
+            temp = triangle[i - 1]
+            
