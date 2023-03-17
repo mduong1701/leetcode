d4cbb7c6ee5840b6d46555b9a249b86e7fee6b1b
Man Duong <mduong8911@hotmail.com>
2023-03-17 07:30:14 -0700

    Just do it

--- day_39/p_34.py
+++ day_39/p_34.py
@@ -5,4 +5,7 @@ class Solution:
     def binarySearch(self, nums, target, leftBias):
         left = 0
         right = len(nums) - 1
-        
+        i = -1
+
+        while left <= right:
+            
