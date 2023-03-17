9291bd0e98275304804a6ae0580f4f5611ae8858
Man Duong <mduong8911@hotmail.com>
2023-03-16 23:44:23 -0700

    Just do it

--- day_38/p_1888.py
+++ day_38/p_1888.py
@@ -1,3 +1,6 @@
 class Solution:
     def minFlips(self, s):
-        
+        # Initialize the final result to be the length of the string
+        result = len(s)
+        # Double the string
+        s = s + s
