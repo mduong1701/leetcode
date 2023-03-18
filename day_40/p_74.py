f1bdf72b81e614f42fc095078bd8d568d099bba2
Man Duong <mduong8911@hotmail.com>
2023-03-18 08:21:35 -0700

    Just do it

--- day_40/p_74.py
+++ day_40/p_74.py
@@ -2,5 +2,8 @@ class Solution:
     def searchMatrix(self, matrix, target):
         top = 0
         bottom = len(matrix)
+        middle = -1
+        while top < bottom:
+            middle = (top + bottom) // 2
 
-        
+            
