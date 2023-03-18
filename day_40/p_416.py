97f03ceaea8568ab9a6d0937cb1488b49ba9b86c
Man Duong <mduong8911@hotmail.com>
2023-03-18 08:11:41 -0700

    Just do it

--- day_40/p_416.py
+++ day_40/p_416.py
@@ -1,25 +1,29 @@
-class Solution:
-    def canPartition(self, nums):
-        numSum = sum(nums)
+def canPartition(nums):
+    numSum = sum(nums)
 
-        if numSum % 2 == 1:
-            return False
-
-        target = numSum / 2
-        dp = set()
-        dp.add(0)
-
-        for i in range(len(nums)):
-            tempDP = set()
-            for num in dp:
-                if num + nums[i] == target:
-                    return True
-
-                if num + nums[i] < target:
-                    tempDP.add(num + nums[i])
-                    tempDP.add(num)
-
-            dp = tempDP
-
+    if numSum % 2 == 1:
         return False
 
+    target = numSum // 2
+    dp = set()
+    dp.add(0)
+
+    for i in range(len(nums)):
+        tempDP = set()
+        for num in dp:
+            tempSum = num + nums[i]
+            if tempSum == target:
+                return True
+
+            tempDP.add(num)
+
+            if tempSum < target:
+                tempDP.add(tempSum)
+
+        dp = tempDP
+
+    return True if target in dp else False
+
+test = [14,9,8,4,3,2]
+# test = [1,5,11,5]
+print(canPartition(test))
