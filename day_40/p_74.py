970e524147a44adae09d9f928c1e2eee1a36df1c
Man Duong <mduong8911@hotmail.com>
2023-03-18 08:26:42 -0700

    Just do it

--- day_40/p_74.py
+++ day_40/p_74.py
@@ -3,10 +3,20 @@ class Solution:
         top = 0
         bottom = len(matrix)
         middle = -1
-        while top < bottom:
+        while top <= bottom:
             middle = (top + bottom) // 2
 
             if target < matrix[middle][0]:
                 bottom = middle - 1
+            elif target > matrix[middle][-1]:
+                top = middle + 1
+            else:
+                break
+
+        l = 0
+        r = len(matrix[0])
+
+        while l <= r:
+            m = (l + r) // 2
 
             
