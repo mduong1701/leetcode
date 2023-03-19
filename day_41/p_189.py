3f29792060288b9a55259ef23ddb77acffd06aaf
Man Duong <mduong8911@hotmail.com>
2023-03-19 07:28:23 -0700

    Just do it

--- day_41/p_189.py
+++ day_41/p_189.py
@@ -1,3 +1,11 @@
 class Solution:
     def rotate(self, nums, k):
-        
+        k = k % len(nums)
+
+        def reverseArray(nums, start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+                
