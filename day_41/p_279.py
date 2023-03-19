4e8a6d01bb806d3a8f1456e3e7feac0653f0408a
Man Duong <mduong8911@hotmail.com>
2023-03-19 07:08:02 -0700

    Just do it

--- day_41/p_279.py
+++ day_41/p_279.py
@@ -3,4 +3,9 @@ class Solution:
         dp = [n for _ in range(n + 1)]
         dp[0] = 0
 
-        
+        for target in range(1, n + 1):
+            for num in range(1, target + 1):
+                square = num ** 2
+                if square > target:
+                    break
+                
