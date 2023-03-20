e993ab508b501eca451ca8de12e31bae5771d4ad
Man Duong <mduong8911@hotmail.com>
2023-03-20 10:03:57 -0700

    Just do it

--- day_42/p_6.py
+++ day_42/p_6.py
@@ -3,4 +3,8 @@ class Solution:
         if numRows == 1:
             return s
 
-        
+        result = ""
+
+        for row in range(numRows):
+            increment = numRows * 2 - 2
+            
