ade112a0aa84e541dba0457a34aba98e0c0382b8
Man Duong <mduong8911@hotmail.com>
2023-03-20 10:16:40 -0700

    Just do it

--- day_42/p_6.py
+++ day_42/p_6.py
@@ -7,4 +7,12 @@ class Solution:
 
         for row in range(numRows):
             increment = numRows * 2 - 2
-            
+            for i in range(row, len(s), increment):
+                result += s[i]
+
+                if row > 0 and \
+                    row < numRows - 1 and \
+                    (i + increment - 2 * row) < len(s):
+                    result += s[i + increment - 2 * row]
+
+        return result
