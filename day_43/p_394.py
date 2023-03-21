ed23913871d4f9c04166dba4b3f49115fdb2b1e6
Man Duong <mduong8911@hotmail.com>
2023-03-21 07:08:24 -0700

    Just do it

--- day_43/p_394.py
+++ day_43/p_394.py
@@ -1,3 +1,9 @@
 class Solution:
     def decodeString(self, s):
-        
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                
