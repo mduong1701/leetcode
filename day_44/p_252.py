b3970459442b2cbdad96972d93204c60256f5736
Man Duong <mduong8911@hotmail.com>
2023-03-22 06:44:22 -0700

    Just do it

--- day_44/p_252.py
+++ day_44/p_252.py
@@ -1,3 +1,9 @@
 class Solution:
     def canAttendMeetings(self, intervals):
-        
+        intervals.sort(key = lambda x: x[0])
+
+        for i in range(1, len(intervals)):
+            interval1 = intervals[i - 1]
+            interval2 = intervals[1]
+
+            
