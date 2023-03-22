760ac93a582f4aeeda5ee692dcc9c0ccd2af5aea
Man Duong <mduong8911@hotmail.com>
2023-03-22 07:22:53 -0700

    Just do it

--- day_44/p_1838.py
+++ day_44/p_1838.py
@@ -2,4 +2,14 @@ class Solution:
     def maxFrequency(self, nums, k):
         left, right, total, result = 0, 0, 0, 0
 
-        
+        while right < len(nums):
+            total += nums[right]
+
+            while nums[right] * (right - left + 1) >= total + k:
+                total -= nums[left]
+                left += 1
+
+            result = max(result, right - left + 1)
+            right += 1
+
+        return result
