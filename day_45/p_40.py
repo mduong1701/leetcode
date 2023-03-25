1aa0e00f2d540b2a1be3f6800943f92479fc35f2
Man Duong <mduong8911@hotmail.com>
2023-03-25 10:30:31 -0700

    Just do it

--- day_45/p_40.py
+++ day_45/p_40.py
@@ -3,4 +3,10 @@ class Solution:
         candidates.sort()
         result = []
 
-        
+        def backtracking(curArr, start, sumLeft):
+            if sumLeft == 0:
+                result.append(curArr.copy())
+            if sumLeft <= 0:
+                return
+
+            
