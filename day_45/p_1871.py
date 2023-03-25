bf7fde1823f1719da4c1eceececdbc3aba700197
Man Duong <mduong8911@hotmail.com>
2023-03-25 09:50:45 -0700

    Just do it

--- day_45/p_1871.py
+++ day_45/p_1871.py
@@ -7,14 +7,14 @@ class Solution:
 
         while q:
             index = q.popleft()
-            start = max(index  + minJump, farthest)
+            start = max(index  + minJump, farthest + 1)
 
-            for current in range(start, min(start + maxJump+ 1, len(s))):
+            for current in range(start, min(index + maxJump + 1, len(s))):
                 if s[current] == "0":
                     q.append(current)
                     if current == len(s) - 1:
                         return True
 
-            farthest = start + maxJump
+            farthest = index + maxJump
 
         return False
