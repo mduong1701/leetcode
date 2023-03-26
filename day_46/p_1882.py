1f72b283f22c18e81075cc179e34ce30c048cd1d
Man Duong <mduong8911@hotmail.com>
2023-03-26 16:41:07 -0700

    Just do it

--- day_46/p_1882.py
+++ day_46/p_1882.py
@@ -1,6 +1,13 @@
 # available = [(weight, index)]
 # unavailable = [(timeLeftToBeFree, weight, index)]
 
+import heapq
+
 class Solution:
     def assignTasks(self, servers, tasks):
-        
+        result = [0 for _ in range(len(tasks))]
+
+        available = [(servers[i], i) for i in range(len(servers))]
+        heapq.heapify(available)
+        unavailable = []
+
