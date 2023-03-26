d1b230abe1b277cb018859fd2e8756dd3ab1c135
Man Duong <mduong8911@hotmail.com>
2023-03-26 15:33:38 -0700

    Just do it

--- day_46/p_148.py
+++ day_46/p_148.py
@@ -43,4 +43,9 @@ class Solution:
                 list2 = list2.next
             tail = tail.next
 
-        
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+
+        return dummy.next
