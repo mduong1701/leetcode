d7bf1b2b4a51d66f86077fad207953aef3c40eec
Man Duong <mduong8911@hotmail.com>
2023-03-28 11:14:34 -0700

    Just do it

--- day_47/p_1498.py
+++ day_47/p_1498.py
@@ -3,4 +3,10 @@ class Solution:
         result = 0
         nums.sort()
 
-        
+        right = len(nums) - 1
+        mod = 10**9 + 7
+
+        for left in range(len(nums)):
+            while (nums[left] + nums[right]) > target and left <= right:
+                right -= 1
+                
