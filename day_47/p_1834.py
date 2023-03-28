2f9ab3be2a1bd0e4ba91c649f25433dac626b8ed
Man Duong <mduong8911@hotmail.com>
2023-03-28 10:00:03 -0700

    Just do it

--- day_47/p_1834.py
+++ day_47/p_1834.py
@@ -1,3 +1,6 @@
 class Solution:
     def getOrder(self, tasks):
-        
+        for i in range(len(tasks)):
+            tasks[i].append(i)
+            
+        tasks.sort(key = lambda task : task[0])
