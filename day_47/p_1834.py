7725b2510532ab993115728f7e50e8a244427856
Man Duong <mduong8911@hotmail.com>
2023-03-28 10:04:50 -0700

    Just do it

--- day_47/p_1834.py
+++ day_47/p_1834.py
@@ -1,6 +1,21 @@
+import heapq
+
 class Solution:
     def getOrder(self, tasks):
         for i in range(len(tasks)):
             tasks[i].append(i)
-            
+
         tasks.sort(key = lambda task : task[0])
+
+        result = []
+        minHeap = []
+
+        i = 0
+        time = 0
+
+        while minHeap or i < len(tasks):
+            while i < len(tasks) and time >= tasks[i][0]:
+                heapq.heappush(minHeap, [tasks[i][1], tasks[i][2]])
+                i += 1
+
+            
