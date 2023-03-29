fe934297678d9771e14c728363dbec65b97b0415
Man Duong <mduong8911@hotmail.com>
2023-03-29 09:44:27 -0700

    Just do it

--- day_48/p_739.py
+++ day_48/p_739.py
@@ -3,4 +3,7 @@ class Solution:
         result = [0 for _ in range(len(temperatures))]
         stack = []
 
-        
+        for i, t in enumerate(temperatures):
+            stack.append((t, i))
+
+        return result
