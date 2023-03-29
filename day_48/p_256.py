ecb43f1e8e47d18c2f984582d7099cdf9307defc
Man Duong <mduong8911@hotmail.com>
2023-03-29 10:21:03 -0700

    Just do it

--- day_48/p_256.py
+++ day_48/p_256.py
@@ -1,3 +1,11 @@
 class Solution:
     def minCost(self, costs):
-        
+        result = costs[0]
+
+        for house in range(1, len(costs)):
+            tempCosts = []
+            for costIndex in range(len(costs[house])):
+                temp = costs[house][costIndex] + min(result[:costIndex], result[costIndex + 1:])
+                tempCosts.append(temp)
+            
+
