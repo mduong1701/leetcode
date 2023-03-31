dddf3d0479722051371939da72ebb6a019f6bde2
Man Duong <mduong8911@hotmail.com>
2023-03-31 11:38:27 -0700

    Just do it

--- day_49/p_1930.py
+++ day_49/p_1930.py
@@ -4,4 +4,17 @@ class Solution:
         left = set()
         right = {}
 
+        for character in s:
+            right[character] = right.get(character, 0) + 1
         
+        for i in range(len(s)):
+            right[s[i]] -= 1
+            
+            if right[s[i]] == 0:
+                del right[s[i]]
+
+            
+
+
+
+            left.add(s[i])
