ea5fb06dc100e20130bfc9ee7b5b010b4a313622
Man Duong <mduong8911@hotmail.com>
2023-03-31 11:43:41 -0700

    Just do it

--- day_49/p_1930.py
+++ day_49/p_1930.py
@@ -9,12 +9,13 @@ class Solution:
         
         for i in range(len(s)):
             right[s[i]] -= 1
-            
+
             if right[s[i]] == 0:
                 del right[s[i]]
 
-            
-
-
+            for j in range(26):
+                c = chr(ord(s[i]) + j)
+                if c in left and c in right:
+                    result.add((s[i], c))
 
             left.add(s[i])
