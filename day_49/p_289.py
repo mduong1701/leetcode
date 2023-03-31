9a4143a6af459c1b8da5d100d3918509f09fef04
Man Duong <mduong8911@hotmail.com>
2023-03-31 13:05:49 -0700

    Just do it

--- day_49/p_289.py
+++ day_49/p_289.py
@@ -1,3 +1,9 @@
 class Solution:
     def gameOfLife(self, board):
+        # Original      New         Value
+        #    0           0            0
+        #    0           1            1
+        #    1           0            2
+        #    1           1            3
+
         
