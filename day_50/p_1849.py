3da9650b5ad90bf889593a2ee973e091885b1b6f
Man Duong <mduong8911@hotmail.com>
2023-04-01 12:14:30 -0700

    Just do it

--- day_50/p_1849.py
+++ day_50/p_1849.py
@@ -1,3 +1,8 @@
 class Solution:
     def splitString(self, s):
+        for i in range(len(s) - 1):
+            value = int(s[ : i + 1])
+            if dfs(i, value):
+                return True
+        return False
         
