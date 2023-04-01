cfaa0cee592b777fd0828b88be6462353fbf2a06
Man Duong <mduong8911@hotmail.com>
2023-04-01 12:20:41 -0700

    Just do it

--- day_50/p_1849.py
+++ day_50/p_1849.py
@@ -1,5 +1,13 @@
 class Solution:
     def splitString(self, s):
+        def dfs(index, prev):
+            if index == len(s):
+                return True
+            for j in range(index, len(s)):
+                value = int(s[index : j + 1])
+                if value + 1 == prev and dfs(j + 1, value):
+                    return True
+            return False
         for i in range(len(s) - 1):
             value = int(s[ : i + 1])
             if dfs(i + 1, value):
