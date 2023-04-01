0ae3b408e1f524c5b0f21c5ee0161fecd8f35f16
Man Duong <mduong8911@hotmail.com>
2023-04-01 12:15:29 -0700

    Just do it

--- day_50/p_1849.py
+++ day_50/p_1849.py
@@ -2,7 +2,6 @@ class Solution:
     def splitString(self, s):
         for i in range(len(s) - 1):
             value = int(s[ : i + 1])
-            if dfs(i, value):
+            if dfs(i + 1, value):
                 return True
         return False
-        
