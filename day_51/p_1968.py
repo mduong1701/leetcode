76a92a22aba50e984194345457e2723ab0553666
Man Duong <mduong8911@hotmail.com>
2023-04-02 10:44:40 -0700

    Just do it

--- day_51/p_1968.py
+++ day_51/p_1968.py
@@ -3,5 +3,8 @@ class Solution:
         # [ 1 2 3 4 5]
         nums.sort()
         result = []
+        left = 0
+        right = len(nums) - 1
 
-        
+        while len(result) != len(nums):
+            
