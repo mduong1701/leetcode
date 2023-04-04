81e6a6d3c3deda90a3545f81e07ce39325af00aa
Man Duong <mduong8911@hotmail.com>
2023-04-03 18:08:28 -0700

    Just do it

--- day_52/p_271.py
+++ day_52/p_271.py
@@ -1,3 +1,6 @@
+from re import I
+
+
 class Codec:
     def encode(self, strs):
         result = ""
@@ -11,4 +14,11 @@ class Codec:
         result = []
         i = 0
 
-        
+        while i < len(s):
+            j = i
+            
+            while j != "#":
+                j += 1
+
+            length = int(s[i : j])
+            
