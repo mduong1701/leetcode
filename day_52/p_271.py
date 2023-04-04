c4581216ec0bad9b115fad6473aba609a43020d2
Man Duong <mduong8911@hotmail.com>
2023-04-03 18:03:20 -0700

    Just do it

--- day_52/p_271.py
+++ day_52/p_271.py
@@ -1,5 +1,10 @@
 class Codec:
-    def encode(self, strs: List[str]) -> str:
+    def encode(self, strs):
+        result = ""
+
+        for s in strs:
+            result += str(len(s)) + "#" + s
         
+        return result
 
     def decode(self, s: str) -> List[str]:
