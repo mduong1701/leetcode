a3ee879bd60f1bf9fac5322c712b2dc5a58b828c
Man Duong <mduong8911@hotmail.com>
2023-04-03 18:12:04 -0700

    Just do it

--- day_52/p_271.py
+++ day_52/p_271.py
@@ -21,4 +21,8 @@ class Codec:
                 j += 1
 
             length = int(s[i : j])
-            
+            result.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+
+        return result
+
