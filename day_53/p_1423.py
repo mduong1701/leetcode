f88df3d0e7893e6fe622e87df34bcb23c3a34e23
Man Duong <mduong8911@hotmail.com>
2023-04-04 21:54:22 -0700

    Just do it

--- day_53/p_1423.py
+++ day_53/p_1423.py
@@ -1,3 +1,8 @@
 class Solution:
     def maxScore(self, cardPoints, k):
+        # cardPoints = [ 1 , 2 , 3 , 4 , 5 , 6 , 1 ], k = 3
+        # index          0   1   2   3   4   5   6
+        #                l               r
+        l = 0
+        r = len(cardPoints) - k
         
