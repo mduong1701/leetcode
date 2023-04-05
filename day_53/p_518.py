0b3f1d265a4f45c8c9fb40ab4a93b2268da3cafd
Man Duong <mduong8911@hotmail.com>
2023-04-04 22:33:51 -0700

    Just do it

--- day_53/p_518.py
+++ day_53/p_518.py
@@ -1,3 +1,9 @@
 class Solution:
-    def change(self, amount: int, coins):
-        
+    def change(self, amount, coins):
+        dp = [0 for _ in range(amount + 1)]
+        dp[0] = 1
+
+        for coinIndex in range(len(coins) - 1, -1, -1):
+            nextDP = [0 for _ in range(amount + 1)]
+
+            
