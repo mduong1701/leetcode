003b3c17f695b1588f656119e4c8d226e388fa10
Man Duong <mduong8911@hotmail.com>
2023-04-04 22:35:14 -0700

    Just do it

--- day_53/p_518.py
+++ day_53/p_518.py
@@ -5,5 +5,7 @@ class Solution:
 
         for coinIndex in range(len(coins) - 1, -1, -1):
             nextDP = [0 for _ in range(amount + 1)]
+            nextDP[0] = 1
 
-            
+            for i in range(1, amount + 1):
+                
