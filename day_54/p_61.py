2dd8a3351924832c9304bbcb767e9a65cd9573d8
Man Duong <mduong8911@hotmail.com>
2023-04-05 23:03:26 -0700

    Just do it

--- day_54/p_61.py
+++ day_54/p_61.py
@@ -22,4 +22,8 @@ class Solution:
         for _ in range(length - k - 1):
             cur = cur.next
 
-        
+        newHead = cur.next
+        cur.next = None
+        tail.next = head
+
+        return newHead
