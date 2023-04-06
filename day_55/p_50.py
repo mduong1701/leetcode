c1ddc9fcec18aebcbeee550fcea3d50c97bad196
Man Duong <mduong8911@hotmail.com>
2023-04-06 11:09:32 -0700

    Just do it

--- day_55/p_50.py
+++ day_55/p_50.py
@@ -1,3 +1,13 @@
 class Solution:
     def myPow(self, x, n):
+        def helper(base, power):
+            if base == 0:
+                return 0
+            if power == 0:
+                return 1
+            
+            result = helper(base, power // 2)
+            result = result * result
+            return base * result if power % 2 else result
+
         
