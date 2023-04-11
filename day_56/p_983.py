5eafe67f8377025d58b9384ec985675cb856b744
Man Duong <mduong8911@hotmail.com>
2023-04-10 17:37:02 -0700

    Just do it

--- day_56/p_983.py
+++ day_56/p_983.py
@@ -3,8 +3,11 @@ class Solution:
         # days = [1, 4, 6, 7, 8, 20], costs = [2, 7, 15], [1, 7, 30]
         #         0  1  2  3  4  5                
         dp = {}
-        #
+        # 5     inf
         for i in range(len(days) - 1, -1, -1):
             dp[i] = float('inf')
-            
+            for day, cost in zip([1, 7, 30], costs):
+                j = i
+                
+
 
