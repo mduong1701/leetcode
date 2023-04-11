446ebe6151e29d0e48f18b72e91990adb81100b0
Man Duong <mduong8911@hotmail.com>
2023-04-10 18:17:06 -0700

    Just do it

--- day_56/p_973.py
+++ day_56/p_973.py
@@ -1,6 +1,14 @@
+import heapq
+
+
 class Solution:
     def kClosest(self, points, k):
         # points = [[3,3],[5,-1],[-2,4]], k = 2
 
         minHeap = []
         
+        for x, y in points:
+            distance = (x ** 2) + (y ** 2)
+            minHeap.append([distance, x, y])
+        
+        heapq.heapify(minHeap)
