d0d285b05eab50aacd31c5f6a6d180014e9fc07d
Man Duong <mduong8911@hotmail.com>
2023-04-10 17:35:21 -0700

    Just do it

--- day_56/p_983.py
+++ day_56/p_983.py
@@ -1,5 +1,10 @@
 class Solution:
     def mincostTickets(self, days, costs):
         # days = [1, 4, 6, 7, 8, 20], costs = [2, 7, 15], [1, 7, 30]
+        #         0  1  2  3  4  5                
+        dp = {}
+        #
+        for i in range(len(days) - 1, -1, -1):
+            dp[i] = float('inf')
+            
 
-        
