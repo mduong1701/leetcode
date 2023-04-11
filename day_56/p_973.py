ba750e7c45a7d92870a12754ef0e2290c612dfe0
Man Duong <mduong8911@hotmail.com>
2023-04-10 18:17:44 -0700

    Just do it

--- day_56/p_973.py
+++ day_56/p_973.py
@@ -6,9 +6,12 @@ class Solution:
         # points = [[3,3],[5,-1],[-2,4]], k = 2
 
         minHeap = []
+        result = []
         
         for x, y in points:
             distance = (x ** 2) + (y ** 2)
             minHeap.append([distance, x, y])
         
         heapq.heapify(minHeap)
+
+        while k > 0:
