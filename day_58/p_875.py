5887c03d2b5ebd38b492a510ace7e14cb14eb993
Man Duong <mduong8911@hotmail.com>
2023-04-12 15:31:41 -0700

    Just do it

--- day_58/p_875.py
+++ day_58/p_875.py
@@ -10,5 +10,14 @@ class Solution:
         while left <= right:
             middle = (left + right) // 2
             time = 0
+
             for pile in piles:
                 time += math.ceil(pile / middle)
+            
+            if time > h:
+                left = middle + 1
+                continue
+            
+            result = min(result, middle)
+            right = middle - 1
+            
