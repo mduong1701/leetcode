9ed69ababc941762466501b62d429cc37ef3d82b
Man Duong <mduong8911@hotmail.com>
2023-04-12 15:26:20 -0700

    Just do it

--- day_58/p_875.py
+++ day_58/p_875.py
@@ -1,3 +1,8 @@
 class Solution:
     def minEatingSpeed(self, piles, h):
+        # piles = [3,6,7,11], h = 8
+        result = float("inf")
+        left = 0
+        right = max(piles)
+
         
