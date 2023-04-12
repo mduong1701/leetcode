858e9a13fe430a8330a1b924fd52de0a5215d8ae
Man Duong <mduong8911@hotmail.com>
2023-04-12 16:57:13 -0700

    Just do it

--- day_58/p_134.py
+++ day_58/p_134.py
@@ -8,4 +8,8 @@ class Solution:
 
         for i in range(len(gas)):
             total += (gas[i] - cost[i])
-            
+            if total < 0:
+                total = 0
+                result = i + 1
+
+        return result
