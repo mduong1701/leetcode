57ab291752ca65c76d3b0141670f99eacb317379
Man Duong <mduong8911@hotmail.com>
2023-04-11 17:57:52 -0700

    Just do it

--- day_57/p_1921.py
+++ day_57/p_1921.py
@@ -1,3 +1,20 @@
+import math
+
 class Solution:
     def eliminateMaximum(self, dist, speed):
-        
+        # dist = [1,3,4], speed = [1,1,1]
+        # minuteReached = [ 1, 3, 4]
+
+        minuteReached = []
+        for i in range(len(dist)):
+            minuteReached.append(math.ceil(dist[i] / speed[i]))
+
+        minuteReached.sort()
+        result = 0
+        for minute in range(len(minuteReached)):
+            if minute >= minuteReached[minute]:
+                return result
+
+            result += 1
+
+        return result
