b7a78e0102fc2cd3278d89ae9e3adc736d3970f0
Man Duong <mduong8911@hotmail.com>
2023-04-13 21:05:53 -0700

    Just do it

--- day_59/p_778.py
+++ day_59/p_778.py
@@ -1,8 +1,16 @@
+import heapq
+
 class Solution:
     def swimInWater(self, grid):
         N = len(grid)
         visited = set()
         minHeap = [[grid[0][0], 0, 0]]
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
 
-        
+        visited.add((0, 0))
+
+        while minHeap:
+            elevation, x, y = heapq.heappop(minHeap)
+
+
 
