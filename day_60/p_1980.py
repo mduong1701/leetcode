466fa2e0e32e9ddb6f78a99210d0434859c10c90
Man Duong <mduong8911@hotmail.com>
2023-04-14 12:06:33 -0700

    Just do it

--- day_60/p_1980.py
+++ day_60/p_1980.py
@@ -3,4 +3,7 @@ class Solution:
         # nums = ["111","011","001"]
         numSet = {num for num in nums}
 
-        
+        def backtracking(index, current):
+            pass
+
+        return backtracking(0, ["0" for _ in range(len(nums))])
