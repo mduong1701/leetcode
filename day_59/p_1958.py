94eabebe0296a926f12296b4cf7903832d82773f
Man Duong <mduong8911@hotmail.com>
2023-04-13 20:18:46 -0700

    Just do it

--- day_59/p_1958.py
+++ day_59/p_1958.py
@@ -1,3 +1,6 @@
 class Solution:
     def checkMove(self, board, rMove, cMove, color):
-        
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        DIRECTIONS = [[ 1, 0],[ -1, 0],[ 0, 1],[ 0, -1],[ 1, 1],[ -1, -1],[ 1, -1],[ -1, 1]]
