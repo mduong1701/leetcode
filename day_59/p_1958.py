de34c7ef381d889d49ab8fb572c44b153877a305
Man Duong <mduong8911@hotmail.com>
2023-04-13 20:20:20 -0700

    Just do it

--- day_59/p_1958.py
+++ day_59/p_1958.py
@@ -4,3 +4,9 @@ class Solution:
         COLS = len(board[0])
 
         DIRECTIONS = [[ 1, 0],[ -1, 0],[ 0, 1],[ 0, -1],[ 1, 1],[ -1, -1],[ 1, -1],[ -1, 1]]
+
+        def move(x, y, color, direction):
+            dx, dy = direction
+            newX, newY = x + dx, y + dy
+
+            
