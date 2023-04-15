20ace57bc530b44edb1e05c95bce033bdc93fb82
Man Duong <mduong8911@hotmail.com>
2023-04-15 10:27:46 -0700

    Just do it

--- day_61/p_787.py
+++ day_61/p_787.py
@@ -1,3 +1,10 @@
 class Solution:
     def findCheapestPrice(self, n, flights, src, dst, k):
-        
+        # n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
+        prices = [float('inf')] * n
+        # [ inf inf inf ]
+        prices[src] = 0
+        # [ 0 inf inf ]
+        for _ in range(k + 1):
+            pricesTemp = prices.copy()
+            
