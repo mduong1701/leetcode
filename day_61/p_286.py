800feaf13031bd8a64ec1a408d2b979394c9a53e
Man Duong <mduong8911@hotmail.com>
2023-04-15 09:27:31 -0700

    Just do it

--- day_61/p_286.py
+++ day_61/p_286.py
@@ -1,6 +1,12 @@
+from collections import deque
+
 class Solution:
     def wallsAndGates(self, rooms):
         ROWS = len(rooms)
         COLUMNS = len(rooms[0])
+        DIRECTIONS = [[1,0],[-1,0],[0,1],[0,-1]]
+
+        visited = set()
+        queue = deque()
 
         
