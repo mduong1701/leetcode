d03d41c8dfe9118ec90c68776601611e8f3df1d3
Man Duong <mduong8911@hotmail.com>
2023-04-15 09:24:30 -0700

    Just do it

--- day_60/p_1985.py
+++ day_60/p_1985.py
@@ -2,10 +2,11 @@ import heapq
 
 class Solution:
     def kthLargestNumber(self, nums, k):
-        maxHeap = heapq.heapify([-1 * int(num) for num in nums])
-        result = 0
+        maxHeap = [-1 * int(num) for num in nums]
+        heapq.heapify(maxHeap)
+        
         while maxHeap:
-            tmp = maxHeap.heappop()
+            tmp = heapq.heappop(maxHeap)
             k -= 1
             if k == 0:
                 return str(-1 * tmp)
