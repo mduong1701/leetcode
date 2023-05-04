62fe5a21b717bfd0a722306086534125e4b79d9d
Man Duong <mduong8911@hotmail.com>
2023-05-04 10:26:53 -0700

    Just do it

--- day_62/p_1984.py
+++ day_62/p_1984.py
@@ -1,4 +1,12 @@
 class Solution:
     def minimumDifference(self, nums, k):
         nums.sort()
+        # 1   4   7   9
+        # l   r
+
+        result = float('inf')
+
+        l = 0
+        r = k - 1
+
         
