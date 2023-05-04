3385a434540240496b110bef652ae706f9a6bffb
Man Duong <mduong8911@hotmail.com>
2023-05-04 10:45:00 -0700

    Just do it

--- day_62/p_2001.py
+++ day_62/p_2001.py
@@ -1,3 +1,8 @@
 class Solution:
     def interchangeableRectangles(self, rectangles):
+        count = {}
+
+        for width, height in rectangles:
+            count[width / height] = 1 + count.get(width / height, 0)
+
         
