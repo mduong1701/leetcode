3abb2b4dbec417afebdaaacc7db5d7a1fc4d6267
Man Duong <mduong8911@hotmail.com>
2023-05-04 10:47:50 -0700

    Just do it

--- day_62/p_2001.py
+++ day_62/p_2001.py
@@ -5,5 +5,10 @@ class Solution:
         for width, height in rectangles:
             count[width / height] = 1 + count.get(width / height, 0)
 
+        result = 0
+
         for times in count.values():
-            
+            if times > 1:
+                result += times * (times - 1) / 2
+
+        return result
