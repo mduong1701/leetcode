6e4d9d959167bba52930965e4525487de4a93f14
Man Duong <mduong8911@hotmail.com>
2023-05-04 10:28:06 -0700

    Just do it

--- day_62/p_1984.py
+++ day_62/p_1984.py
@@ -9,4 +9,9 @@ class Solution:
         l = 0
         r = k - 1
 
-        
+        while r < len(nums):
+            result = min(result, nums[r] - nums[l])
+            l += 1
+            r += 1
+
+        return result
