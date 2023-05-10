72468ac7f6f439549702d2563aa03af6a7ca4a2e
Man Duong <mduong8911@hotmail.com>
2023-05-09 21:44:14 -0700

    Just do it

--- day_63/p_554.py
+++ day_63/p_554.py
@@ -1,3 +1,10 @@
 class Solution:
     def leastBricks(self, wall):
-        
+
+        countGap = {0: 0}
+
+        for row in wall:
+
+            length = 0
+            for brick in row:
+                
