53a022e087a5a9b7890d8000d63019563f9bfa9f
Man Duong <mduong8911@hotmail.com>
2023-05-10 22:23:09 -0700

    Just do it

--- day_64/p_1899.py
+++ day_64/p_1899.py
@@ -6,4 +6,8 @@ class Solution:
             if triplet[0] > target[0] or triplet[1] > target[1] or triplet[2] > target[2]:
                 continue
 
-            
+            for index, value in enumerate(triplet):
+                if value == target[index]:
+                    result.add(index)
+
+        return len(result) == 3
