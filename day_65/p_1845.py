859ec58b600ecbcb44df7f2de132b35f0934c347
Man Duong <mduong8911@hotmail.com>
2023-05-11 21:42:38 -0700

    Just do it

--- day_65/p_1845.py
+++ day_65/p_1845.py
@@ -1,10 +1,12 @@
+import heapq
+
 class SeatManager:
 
     def __init__(self, n: int):
         self.unreserved = [i for i in range(1, n + 1)]
 
     def reserve(self) -> int:
-        
+        return heapq.heappop(self.unreserved)
 
     def unreserve(self, seatNumber: int) -> None:
         
