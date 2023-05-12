b40e68ffd12289e9b94b893c17758973432b2b72
Man Duong <mduong8911@hotmail.com>
2023-05-12 10:03:54 -0700

    Just do it

--- day_66/p_2013.py
+++ day_66/p_2013.py
@@ -12,3 +12,11 @@ class DetectSquares:
         self.points.append(point)
 
     def count(self, point):
+        result = 0
+        px, py = point
+
+        for x, y in self.points:
+            if abs(x - px) != abs(y - py) or px == x or py == y:
+                continue
+
+            
