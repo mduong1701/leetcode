3ab1629426cc0cf95bed06be98093136e38adf8b
Man Duong <mduong8911@hotmail.com>
2023-05-11 22:05:05 -0700

    Just do it

--- day_65/p_846.py
+++ day_65/p_846.py
@@ -1,9 +1,14 @@
+import heapq
+
 class Solution:
     def isNStraightHand(self, hand, groupSize):
-
         count = {}
 
         for card in hand:
             count[card] = 1 + count.get(card, 0)
 
+        minHeap = list(count.keys)
+        heapq.heapify(minHeap)
+
         
+
